004a94da5ae2a0e28d4da1e0355286f8ea51e8a1
Eunno An <12171647@inha.edu>
2021-12-08 23:18:01 +0900

    Update 6. zigzag-conversion.cpp

--- 6. zigzag-conversion.cpp
+++ 6. zigzag-conversion.cpp
@@ -2,6 +2,8 @@
 //예외처리가 중요한 구현 문제.
 //Runtime: 4 ms, faster than 97.86% of C++ online submissions for Zigzag Conversion.
 
+//잘 구현했지만 시간이 너무 오래걸렸음. 40~50분 정도.
+//그것보다 직관적인 구현들을 아래에 적어놨으니 볼것.
 class Solution {
 public:
     string convert(string s, int numRows) {
@@ -38,4 +40,20 @@ public:
     }
     return ret;
 }
+};
+
+//진짜 지그재그로 가는것을 구현함.
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string ans;
+        vector <string> tmp(numRows);                                                         //create strings for each row
+        int i=0, n=s.size();
+        while(i<n){                                      
+            for(int j=0;j<numRows && i<n; j++, i++) tmp[j] += s[i];               //add for our strings character when we move down
+            for(int j=numRows - 2;j>0 && i<n; j--, i++) tmp[j] += s[i];            //add for our strings character when we move up exept first and last string
+        }
+        for(int j=0;j<numRows; j++) ans += tmp[j];                                       //combain together our strings
+        return ans;
+    }
 };
