a049056793617bd12bb9852228db3144ff6d52ed
Eunno An <12171647@inha.edu>
2021-12-12 23:41:07 +0900

    Create 14. longest-common-prefix.cpp

---
+++ 14. longest-common-prefix.cpp
@@ -0,0 +1,4 @@
+//Success
+//이 부분은 공부할게 많은 부분임. 전체 문자열들의 공통 접두사를 찾는 문젠데, 해결방법이 정말 다양하다.
+//기존 것도 BadCodes에 옮겨놨음.
+//Runtime: 104 ms, faster than 5.10% of C++ online submissions for Longest Common Prefix.
