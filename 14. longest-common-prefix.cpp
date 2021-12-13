e6b888b015e89ec239439a9d1f869dfcad4f27db
Eunno An <12171647@inha.edu>
2021-12-13 22:56:05 +0900

    Update 14. longest-common-prefix.cpp

--- 14. longest-common-prefix.cpp
+++ 14. longest-common-prefix.cpp
@@ -67,3 +67,39 @@ public String longestCommonPrefix(String[] strs) {
     }
     return strs[0];//전체 문자열이 다 같을 경우!
 }
+
+
+//네번째 해결 방법: BadCodes
+//분할 정복!
+//Runtime: 4 ms, faster than 71.16% of C++ online submissions for Longest Common Prefix.
+//Memory Usage: 12.3 MB, less than 8.48% of C++ online submissions for Longest Common Prefix.
+//IF문 실수 했던것 잘 보자!
+class Solution {
+public:
+    string getPrefix(string a, string b){
+        string ret = "";
+        for(int i=0; i<min(a.size(), b.size()); i++){
+            if(a[i] != b[i])
+                return ret;
+            ret += a[i];
+        }
+        return ret;
+    }
+    string divideNConquer(int start, int end, vector<string>& strs){
+        
+        if(start == end)
+            return strs[start];
+        //아래 코드는 start == end일 때를 처리할 수 없다.
+        //if(start + 1 == end){
+        //    return getPrefix(strs[start], strs[end]);
+        //}
+        
+        int mid = (start + end)/2;
+        string leftP = divideNConquer(start ,mid, strs);
+        string rightP = divideNConquer(mid+1, end, strs);
+        return getPrefix(leftP, rightP);
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        return divideNConquer(0, strs.size()-1, strs);
+    }
+};
