6b123c7e73edf857d32b0418eb9e5a1aba9b231a
Eunno An <12171647@inha.edu>
2021-12-22 23:33:04 +0900

    Create 16. 3Sum Closet.cpp

---
+++ 16. 3Sum Closet.cpp
@@ -0,0 +1,24 @@
+//fail
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int sum = nums[0] + nums[1] + nums[2];//initial sum
+        for(int i=0; i<n-2; i++){
+            int j = i+1;
+            int k = n-1;
+            while(j < k){
+                int temp = nums[i] + nums[j] + nums[k];
+                if(abs(temp-target) < abs(sum-target)) sum = temp;//거리가 적으면 이 sum으로 교체
+                if(temp>target)
+                    k--;
+                else if(temp<target)
+                    j++;
+                else
+                    return target;
+            }
+        }
+        return sum;
+    }
+};
