42abe282bc904941217c1ef7bcd376cfa0e95b35
Eunno An <12171647@inha.edu>
2021-12-29 23:35:08 +0900

    Update Remove Element.cpp

--- Remove Element.cpp
+++ Remove Element.cpp
@@ -16,3 +16,19 @@ public:
         return i;
     }
 };
+//erase와 remove를 같이 쓴 경우. 근데 이럴 필요가 없다.
+class Solution {
+public:
+    static int removeElement(std::vector<int>& nums, int val) {
+        const auto it = std::remove(nums.begin(), nums.end(), val);
+        return std::distance(nums.begin(), it);
+    }
+};
+//erase는 쓸 필요가 없다. 왜냐하면 val과 같은 요소들을 뒤로 옮기는건 아무래도 상관이 없기 때문이다.
+class Solution {
+public:
+    static int removeElement(std::vector<int>& nums, int val) {
+        const auto it = std::remove(nums.begin(), nums.end(), val);
+        return std::distance(nums.begin(), it);
+    }
+};
