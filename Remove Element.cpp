7ccc611a071387602f2fa07f81d34cbb4e1da578
Eunno An <12171647@inha.edu>
2021-12-29 23:43:00 +0900

    Update Remove Element.cpp

--- Remove Element.cpp
+++ Remove Element.cpp
@@ -28,6 +28,8 @@ public:
 //erase는 쓸 필요가 없다. 왜냐하면 val과 같은 요소들을 뒤로 옮기는건 아무래도 상관이 없기 때문이다.
 //distance(): http://www.cplusplus.com/reference/iterator/distance/?kw=distance
 //iterator간의 거리를 리턴한다!!
+//remove(): http://www.cplusplus.com/reference/algorithm/remove/?kw=remove
+//10 20 30 30 20 10 10 20 -> remove(vec.begin(), vec.end(), 20) -> 10 30 30 10 10 ? ? ?가 되고, remmove가 반환하는 값은 맨 왼쪽 물음표가 된다.
 class Solution {
 public:
     static int removeElement(std::vector<int>& nums, int val) {
