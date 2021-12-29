71943b1f3ef3cae0250b50f92edc0cdcd64e6523
Eunno An <12171647@inha.edu>
2021-12-29 23:38:01 +0900

    Update Remove Element.cpp

--- Remove Element.cpp
+++ Remove Element.cpp
@@ -17,14 +17,17 @@ public:
     }
 };
 //erase와 remove를 같이 쓴 경우. 근데 이럴 필요가 없다.
+
 class Solution {
 public:
-    static int removeElement(std::vector<int>& nums, int val) {
-        const auto it = std::remove(nums.begin(), nums.end(), val);
-        return std::distance(nums.begin(), it);
+    int removeElement(vector<int>& nums, int val) {
+            nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+        return nums.size();
     }
 };
 //erase는 쓸 필요가 없다. 왜냐하면 val과 같은 요소들을 뒤로 옮기는건 아무래도 상관이 없기 때문이다.
+//distance(): http://www.cplusplus.com/reference/iterator/distance/?kw=distance
+//iterator간의 거리를 리턴한다!!
 class Solution {
 public:
     static int removeElement(std::vector<int>& nums, int val) {
