7d0e9c3be9544aa545c852d4384197fcffa56c56
Eunno An <12171647@inha.edu>
2022-01-02 19:50:23 +0900

    Create Search in Rotated Sorted Array.cpp

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,55 @@
+Success
+Time: 40분
+Runtime: 7 ms, faster than 19.23% of C++ online submissions for Search in Rotated Sorted Array.
+Memory Usage: 13.1 MB, less than 26.74% of C++ online submissions for Search in Rotated Sorted Array.
+  
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        int k = findTarget(nums, start, end, target);
+        return k;
+    }
+
+    //findTarget(vector<int> nums, int target, int start, int end)//start와 end가 주어지면 nums[start ]~ nums[end]까지 이분탐색으로 target을 찾는다.
+    int findTarget(vector<int> nums, int start, int end, int target) {
+        if (start == end) {
+            if (nums[start] == target)
+                return start;
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (nums[start] > nums[end]) {
+            int left = findTarget(nums, start, mid, target);
+       
+            if (left != -1 && nums[left] == target)
+                return left;
+            int right = findTarget(nums, mid + 1, end, target);
+            if (right != -1 &&  nums[right] == target)
+                return right;
+            return -1;
+        }
+        else {
+            while (start < end) {
+                mid = (start + end) / 2;
+                if (nums[mid] < target)
+                    start = mid + 1;
+                else if (nums[mid] > target)
+                    end = mid - 1;
+                else
+                    return mid;
+            }
+            if (nums[start] == target)
+                return start;
+            else
+                return -1;
+        }
+        return 123456789;//"OOPS"
+    }
+};
