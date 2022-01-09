93a7e49ad51b58b522dc166dd38efe50541bfd9c
Eunno An <12171647@inha.edu>
2022-01-09 19:50:57 +0900

    Update Result.cpp

--- Weekly Contest 275/Result.cpp
+++ Weekly Contest 275/Result.cpp
@@ -1,5 +1,7 @@
 2133. Check if Every Row and Column Contains All Numbers
 이건 스도쿠 문제와 매우 유사하게 접근함. 단 하나의 실수는 row(3),col(3)으로 선언해서..
+더 좋은 방법 찾음. XOR의 성질과 sum 체크를 이용해서 푸는 방법이 있었음.
+/*내 풀이*/
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
@@ -19,6 +21,36 @@ public:
         return true;
     }
 };
+/*XOR 풀이*/
+class Solution {
+public:
+    bool checkValid(vector<vector<int>> &v) {
+        int xor1 = 0, xor2 = 0,n = v.size();
+        int colsum = 0, rowsum = 0,reqsum = (n*(n+1))/2;
+        if(n<=1) {
+            return true;
+        }
+        for(int i=0;i<n;i++) {
+            xor1 = 0;    xor2 = 0;
+            colsum = 0;  rowsum = 0;
+            for(int j=0;j<n;j++) {
+				// Checking xor column wise
+                xor1 = xor1^(v[i][j]^(j+1));
+                colsum = colsum + v[i][j];
+				// Checking xor row wise
+                xor2 = xor2^(v[j][i]^(j+1));
+                rowsum = rowsum + v[j][i];
+            }
+			// Checking whether  rowsum and columnsum is equal to sum of all 
+			// numbers from 1 to n
+            if(xor1 || xor2 || rowsum != reqsum || colsum != reqsum) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
 2134. Minimum Swaps to Group All 1's Together II
 못품. 이건 아얘 접근 자체를 잘못함.
 
