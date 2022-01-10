39de78f70af1aa7dabf7ff976d07d5880efbab3f
Eunno An <12171647@inha.edu>
2022-01-10 22:18:20 +0900

    Update Combination Sum II.cpp

--- Combination Sum II.cpp
+++ Combination Sum II.cpp
@@ -34,3 +34,39 @@ public:
         return result;
     }
 };
+
+
+//다음날 다시 짜보니까 이렇게 됨
+//실수 한 부분은 i+1 이 아닌 index+1로 해줬음
+//이런 실수 자주하는듯!!!
+//다시 푸니 16분
+class Solution {
+public:
+    vector<vector<int>> ret;
+    void help(vector<int>& candidates, vector<int> now, int index, int target){
+        if(target < 0)
+            return;
+        if(target == 0){
+            ret.push_back(now);
+            return;
+        }
+        for(int i=index; i<candidates.size(); i++){
+            if(i == index || candidates[i-1]!=candidates[i]){
+                now.push_back(candidates[i]);
+                help(candidates, now, i+1, target-candidates[i]);//index+1이 아니라 i+1로 해줘야함. 이 실수 자주하는듯?
+                now.pop_back();  
+            }
+        }
+        return;
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        //유니크한 조합
+        //candidates에 있는 것은 꼭 한번씩 만 쓰여야 한다.
+        sort(candidates.begin(), candidates.end());
+        help(candidates, vector<int>(), 0, target);
+        return ret;
+    }
+};
+
+//1 1 2 5 6 7 10
+//1 
