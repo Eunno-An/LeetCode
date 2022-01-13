c9e364e07fdacb4a784abebf6cf1a01139390c50
Eunno An <12171647@inha.edu>
2022-01-13 19:50:10 +0900

    Update longest-substring-without-repeating-characters.cpp

--- longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,42 @@
-//fail
-//문제점? 첨부터 너무 편법적으로 풀려고 함. 
-//브루트포스 방법부터 생각해 보고, 중복을 어떻게 줄일 수 있는지 체계적으로 생각해 나가자.
-//종만북에서도 강조했던 내용.
-
-//그 외 유의깊게 볼점
-//문자를 바로 인덱스에 대응시키는 방법을 보자. 굉장히 편하다.
-
-//Time Complexity: O(2 * n)
-//O(n)으로 최적화 시키는 방법에 대해 생각해 볼것.
-
-
-
+//fail. 
+//(2022.01.13)2차 복습 fail
 class Solution {
 public:
+    /*
+    문제: 어떤 문자열이 주어질 때, 반복되는 문자가 없는 가장 긴 부분문자열의 길이를 구하여라.
+    
+    해결 아이디어: 어려운 것부터 시작해보자.
+    1)BruteForce
+    인덱스 i부터 j까지 2중 for문을 통해 인덱스를 뽑고,
+    s[i:j]에 반복된 문자열이 있나 확인한다.
+    총 O(n^3)이 든다.
+    
+    2)Sliding Window
+    특정 구간만 보면 된다. 그 구간이 어떤 구간인가? 문자열을 하나씩 보다보면 이 문자열이 두번 등장할 때를 쉽게 알 수 있다. vector<int>chars(128)에 각 문자열의 빈도 횟수를 저장하면 해결할 수 있다.
+    그렇다면 거기서 ans = max(ans, r-l+1)을 통해 쉽게 해결할 수 있다.
+    
+    실수한 점: 
+    
+    예시가 소문자라고 해서, 
+    문제를 잘 읽지 않고 소문자만 있을 것이라고 생각하였음. vector<int> chars(128)을 통해 모든 캐릭터에 대한 배열을 만듬.
+    
+    해결방법이 쉽게 떠오르지 않을떄는, 쉬운 해결방법부터 생각하기.
+    */
     int lengthOfLongestSubstring(string s) {
-        
         int left = 0, right = 0;
         int ret = 0;
         vector<int> chars(128);
-        
         while(right < s.length()){
             char r = s[right];
             chars[r]++;
-            
-            while(chars[r] > 1){
+            while(chars[r]>1){
                 char l = s[left];
                 chars[l]--;
                 left++;
             }
-            ret = max(ret, right - left + 1);
+            ret = max(ret, right-left+1);
             right++;
         }
         return ret;
     }
 };
-
