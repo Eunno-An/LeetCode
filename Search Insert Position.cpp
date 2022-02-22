61a87df9d5c06a04d097ed6ae751ca2c45b6fb01
Eunno An <12171647@inha.edu>
2022-02-22 19:11:41 +0900

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -2,6 +2,11 @@ Success
 Runtime: 4 ms, faster than 80.03% of C++ online submissions for Search Insert Position.
 Memory Usage: 9.8 MB, less than 21.60% of C++ online submissions for Search Insert Position.
   
+2차
+Time: 08분 44초
+Runtime: 3 ms, faster than 90.18% of C++ online submissions for Search Insert Position.
+Memory Usage: 9.6 MB, less than 73.72% of C++ online submissions for Search Insert Position.
+
 class Solution {
 public:
   //return distance(nums.begin() , lower_bound(nums.begin(), nums.end(), target))
@@ -24,5 +29,32 @@ public:
             return start;
         else 
             return start+1;
+    }
+};
+
+2차코드첨부
+class Solution {
+public:
+    /*
+    문제:
+    각기 다른 정수들이 정렬된 배열과 target value가 주어지면
+    target이 찾아지면 그 인덱스를 반환해라.
+    그렇지 않으면 그게 있을만한 곳을 리턴해라.
+    
+    
+    */
+    int searchInsert(vector<int>& nums, int target) {
+        int start =0, end = nums.size()-1;
+        int mid;
+        while(start < end){
+            mid = (start + end)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+        return nums[start] < target ? start+1 : start;
     }
 };
