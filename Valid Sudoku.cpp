43f513fa5f7d451c4ff0b5020197955f7e6f8265
Eunno An <12171647@inha.edu>
2022-02-23 18:53:45 +0900

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -1,12 +1,23 @@
+1차
 Success
 Time: 20분
 Runtime: 24 ms, faster than 56.58% of C++ online submissions for Valid Sudoku.
 Memory Usage: 18.6 MB, less than 58.38% of C++ online submissions for Valid Sudoku.
 
+2차 2022-02-23
+Success
+Time: 11
+Details 
+Runtime: 25 ms, faster than 71.45% of C++ online submissions for Valid Sudoku.
+Memory Usage: 18.6 MB, less than 60.98% of C++ online submissions for Valid Sudoku.
+2차 풀이도 참고하기.
+
 //이것도 좋은 풀이가 있다. 아래에 참조하였음.
 //모범 풀이 아이디어: row와 col, 그리고 3by3 정사각형도 하나의 set이라고 생각하자!. 
 //그럼 bloack[(i/3 * 3 + j/3]으로 큰 스도쿠판을 작은 스도쿠판 9개로 매핑할 수 있다. 
 //큰 실수한 점. board[i][j] 는 char형이다. int num = board[i][j] - '0'; 해줘야함.
+    
+    
 class Solution {
 public:
     bool Rule_1(vector<vector<char>>& board){
@@ -88,6 +99,63 @@ public:
             }
         }
         
+        return true;
+    }
+};
+
+//2차풀이
+class Solution {
+public:
+    /*
+    문제:
+    
+    미완성 스도쿠 보드가 주어지면
+    이게 타당한지 아닌지를 나타내라.
+    
+    회고:
+    더 깔끔하게 했다고 생각했는데 역시 잘짜는 사람은 많구나.
+    시간을 그래도 1/2로 줄였다.
+    
+    */
+    bool isValidRow(vector<vector<char>>& board, int row){
+        vector<int> one2nine(10, 0);
+        for(int i=0; i<board.size(); i++)
+            if(board[row][i] != '.' && one2nine[board[row][i]-'0']++)
+                return false;
+        return true;
+    }
+    bool isValidCol(vector<vector<char>>& board, int col){
+        vector<int> one2nine(10, 0);
+        for(int i=0; i<board.size(); i++)
+            if(board[i][col] != '.' && one2nine[board[i][col]-'0']++)
+                return false;
+        return true;
+    }
+    bool isValidBox(vector<vector<char>>& board, int row, int col){
+        vector<int> one2nine(10, 0);
+        for(int i=row; i<row+3; i++){
+            for(int j=col; j<col+3; j++){
+                if(board[i][j] != '.' && one2nine[board[i][j]-'0']++)
+                    return false;
+            }
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row =0 , col = 0;
+        for(int i=0; i<board.size(); i++){
+            if(!isValidRow(board, i))
+                return false;
+            if(!isValidCol(board, i))
+                return false;
+            if(!isValidBox(board, row, col))
+                return false;
+            col+=3;
+            if(col == 9){
+                row+=3;
+                col = 0;
+            }
+        }
         return true;
     }
 };
