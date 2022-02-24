0fbb01e64f6b85fb283855f04481b97ebcfea2b5
Eunno An <12171647@inha.edu>
2022-02-24 18:48:32 +0900

    Update Combination Sum.cpp

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -4,6 +4,14 @@
 //실제로 sorting 안하고 푸는게 조금 더 빠름.
 //조금 더 최적화를 하려면, sum을 쓰지 말고 target에서 뺴는 식으로 해도 됨.
 //DP 풀이도 첨부하였음. 근데 DP는 코드가 잘 이해가 안간다. 어떻게 이렇게 짜는거지 
+
+//2차
+//시간: 7분 37초
+//풀이 방법은 1차와 같음. 그러나 좀더 깔끔함. 
+//하지만 DP는 생각하지 못했음.
+
+
+//1차
 Runtime: 45 ms, faster than 27.62% of C++ online submissions for Combination Sum.
 Memory Usage: 15.5 MB, less than 37.75% of C++ online submissions for Combination Sum.
   
@@ -38,7 +46,44 @@ public:
         
     }
 };
-
+//2차
+class Solution {
+public:
+    /*
+    문제:
+    candidates라는 서로다른 정수들이 있는 배열들이 주어지고, target이라는 숫자가 주어지면
+    candidates의 합이 target이 되는 unique combinations들을 구해라.
+    candidates의 숫자들은 중복되어서 선택될 수 있다.
+    
+    
+    제한:
+    candidates의 길이는 30 이하.
+    
+    해결방법:
+    1. 브루트포스
+    
+    */
+    vector<vector<int>> ret;
+    void getCombi(vector<int>& candidates, vector<int> temp, int idx, int sum, int target){
+        if(target == sum){
+            ret.push_back(temp);
+            return;
+        }
+        for(int i=idx; i<candidates.size(); i++){
+            if(sum + candidates[i] <= target){
+                temp.push_back(candidates[i]);
+                getCombi(candidates, temp, i, sum+candidates[i], target);
+                temp.pop_back();
+            }
+        }
+        return;
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        getCombi(candidates, vector<int>(), 0, 0, target);
+        return ret;
+    }
+};
+//모범풀이
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
