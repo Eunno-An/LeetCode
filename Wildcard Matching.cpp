825040b3fe70fae67e7a3844b2c327e7f3cceab0
Eunno An <12171647@inha.edu>
2022-02-28 19:20:04 +0900

    Update Wildcard Matching.cpp

--- Wildcard Matching.cpp
+++ Wildcard Matching.cpp
@@ -1,4 +1,5 @@
-
+1차:Fail
+2차:Fail 2022-02-28
 /*
 Fail
 문제: s와 p가 주어지면, p 정규식으로 s 표현이 가능한지 여부를 물어보는 문제
@@ -19,6 +20,8 @@ It is guaranteed for each appearance of the character '*', there will be a previ
 
 3)Backtracking
 무슨 start로 하는게 있다는데 잘 모르겠다.. 이건 좀 더 이해해야겠다.
+
+
 */
 class Solution {
 public:
@@ -58,3 +61,54 @@ public:
 "*?*"
 
 */
+
+
+class Solution {
+public:
+    /*
+    문제:
+    s라는 문자열과 p라는 패턴이 주어지면, 
+    wildcard 패턴 매칭을 구현하라
+    ?: 어떤 문자도 가능하다.(문자 단순히 하나만!)
+    *: 어떤 문자열도 가능하다. 빈 문자열도 가능하다.
+    매칭은 반드시 전체 string을 커버해야 한다.
+    
+    
+    제한:
+    s와 p의 길이는 2000이하이다.
+    
+    해결방법:
+    
+    회고:
+    s가 p보다 길이가 작은 경우
+    
+    
+    
+    */
+    char cache[2001][2001];
+    bool help(int si, int pi, string s, string p){
+        if(pi == p.size())
+            return si == s.size();
+        char& ret = cache[si][pi];
+        if(ret != -1)
+            return ret;
+        
+        if(si == s.size()){
+            if(p[pi] == '*')
+                return help(si, pi+1, s, p);
+            else
+                return ret = false;
+        }
+        
+        if(p[pi]=='?' || s[si] == p[pi])
+            return ret = help(si+1, pi+1, s, p);
+        if(p[pi]=='*')
+            return ret = help(si, pi+1, s, p) || help(si+1, pi, s, p);
+        return ret = false;
+    }
+    
+    bool isMatch(string s, string p) {
+        memset(cache, -1, sizeof(cache));
+        return help(0, 0, s, p);
+    }
+};
