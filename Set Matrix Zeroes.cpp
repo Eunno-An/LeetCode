d7223c700c7a207f588f423040e2c00c71268634
Eunno An <12171647@inha.edu>
2022-03-01 23:53:40 +0900

    Create  Set Matrix Zeroes.cpp

---
+++ Set Matrix Zeroes.cpp
@@ -0,0 +1,40 @@
+/*
+2022-03-01
+Fail.
+공간을 O(m+n)에 해결하라는 Followup을
+시간을 그때 풀으라는 줄 알고 잘못풀었다.
+..
+ㅅㅂ
+*/
+void setZeroes(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        bool firstRow = false;  // do we need to set first row zero
+        bool firstCol = false;  // do we need to ser first col zero
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+					if(a[i][j] == 0){   // store rows and cols state in first row and col
+                    if(i==0) firstRow = true;
+                    if(j==0) firstCol = true;
+                    a[i][0] = 0;
+                    a[0][j] = 0;
+                }
+            }
+        }
+        // cout<<firstRow<<" "<<firstCol<<endl;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(a[i][0] == 0 || a[0][j] == 0){
+                    a[i][j] = 0;
+                }
+            }
+        }
+        
+        if(firstRow){
+            for(int i=0;i<m;i++) a[0][i] = 0;
+        }
+        
+        if(firstCol){
+            for(int i=0;i<n;i++) a[i][0] = 0;
+        }
+    }
