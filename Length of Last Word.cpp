77d593ff92412696624a00dda8e9a8972f85d32c
Eunno An <12171647@inha.edu>
2022-03-29 19:16:24 +0900

    Update Length of Last Word.cpp

--- Length of Last Word.cpp
+++ Length of Last Word.cpp
@@ -1,4 +1,5 @@
-//
+1차 Success
+2차 Success 22-03-29 
 class Solution {
 public:
     /*
@@ -9,6 +10,9 @@ public:
     그래서 그냥 순회하는 방법으로.
     마지막에 temp가 안비었을 경우도 생각해주어야한다.
     굳이 bucket으로 안하고 maxLength만 구하는 것이므로 메모리는 안써도 될 것 같다.
+    
+    회고:
+        ->2차는 s를 연속된 공백을 공백 하나로 만드는 코드로 수정하였ㅇ므.
     */
     int lengthOfLastWord(string s) {
         string temp="";
