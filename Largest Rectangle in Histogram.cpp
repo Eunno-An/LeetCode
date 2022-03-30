ff82c5bc86233e688be16c8a9ca0cd4447f48060
Eunno An <12171647@inha.edu>
2022-03-30 22:52:47 +0900

    Update Largest Rectangle in Histogram.cpp

--- Largest Rectangle in Histogram.cpp
+++ Largest Rectangle in Histogram.cpp
@@ -1,26 +1,29 @@
 1차 Fail
 이유?
   아얘 풀지를 못함. 많이 본 문젠데 확실히 어렵다. 15~18번째줄이 진짜 중요하다.
+  stack에 인덱스를 넣은것과
+  heights의 마지막에 0을 넣어줌으로써 stack이 비어있지 않은 경우를 처리해준 점.
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int maxArea = 0;
         stack<int> st;
-        int ret = 0;
         
-        heights.push_back(0);
-        //0을 넣은 이유는 코드를 좀 쉽게 하기 위해서임.
-        
-        for(int i=0; i<heights.size(); i++){
-            while(!st.empty() && heights[st.top()]>heights[i]){
-                int top = heights[st.top()];
-                st.pop();
-                int ran = st.empty()? -1 : st.top();
-                ret = max(ret, top * (i-ran-1));
+        for(int i = 0; i <= n; i++){
+            int currHeight = i == n ? 0 : heights[i];
+            // check if currHeight becomes greater then height[top] element of stack. we do a push because it's an increasing sequence
+            // otherwise we do pop and find area, so for that we write a while loop
+            while(!st.empty() && currHeight < heights[st.top()]){
+                int top = st.top(); st.pop(); // current element on which we are working
+                // now we need width & area
+                int width = st.empty() ? i : i - st.top() - 1; // width differ if we stack is empty or not empty after we pop the element
+                int area = heights[top] * width; // current height * width
+                maxArea = max(area, maxArea);
             }
+            // if it doesn't enter in while loop, it means it's an increasing sequence & we need to push index
             st.push(i);
         }
-        
-        return ret;
-        
+        return maxArea;
     }
 };
