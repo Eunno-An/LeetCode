9da3146e6de8724353cd542d510d33481de95c16
Eunno An <12171647@inha.edu>
2022-04-02 23:05:27 +0900

    Update Merge Sorted Array.cpp

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -1,5 +1,7 @@
 1차 2022-03-31 Fail
-실패의 원인.. 이런문제가 처음이었던거 같다. 집중도 못함.
+실패의 원인.. 이런문제가 처음이었던거 같다. 집중도 못함
+2차 2022-04-02 Success
+
 class Solution {
 public:
     /*
@@ -29,6 +31,46 @@ public:
 	    {
 		    nums1[k--]=nums2[j--];
 	    }
+    }
+    //2차
+    /*
+    왜 x는 안훑어도 될까?
+    if you start with this:
+
+nums1 = [4,5,6,0,0,0]
+             |     |
+             i     k
+
+nums2 = [1,2,3]
+             |
+             j
+you will end up here
+
+nums1 = [4,5,6,4,5,6]
+       |     |
+       i     k
+
+nums2 = [1,2,3]
+             |
+             j
+	     이 예시를 보면 알 수 있다.
+    */
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k = nums1.size()-1;
+        int x = m-1, y = n-1;
+        while(x>=0 && y>=0){
+            //cout << "First\n";
+            if(nums1[x] < nums2[y])
+                nums1[k--]=nums2[y--];
+            else
+                nums1[k--]=nums1[x--];
+            //printNum(nums1);
+        }
+        while(y>=0){
+            //cout << "Third\n";
+            nums1[k--]=nums2[y--];
+            //printNum(nums1);
+        }
     }
     
 };
