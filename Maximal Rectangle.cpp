afc8099a41ad642f3da8ad29d3765d0f6e82a5ed
Eunno An <12171647@inha.edu>
2022-04-17 19:16:29 +0900

    Update Maximal Rectangle.cpp

--- Maximal Rectangle.cpp
+++ Maximal Rectangle.cpp
@@ -1,5 +1,6 @@
 1차 22-03-30Fail
-
+2차 22-04-17 반은 성공, 반은 맞음.
+    회고: 아이디어는 생각했으나..역시나 maximal histo 구하는거에서 자세한 부분에 대한 구현이 생각이 안나스 그만..
 
 class Solution {
 public:
@@ -15,63 +16,39 @@ public:
     2)histogram에서 최대넓이문제를 응용한 진짜 멋있는 풀이
     ->각각의 column을 순회하면서, histogram에서 maxArea를 구하는 함수를 호출.
     ->새 column을 볼 때마다 histogram갱신. 1이면 높이 추가. 0이면 0으로 만들어버림.
-    */    
-    int largestArea(vector<int>& histogram){
-        int n=histogram.size(), area=0;
+    */  
+    int getMaxRectangle(vector<int>& histo){
         stack<int> s;
-        
-        for(int i=0; i<n; i++){
-            while(!s.empty() && histogram[s.top()]>=histogram[i]){
-                int top = s.top();
-                s.pop();
-                
-                int start;
-                if(s.empty())
-                    start = -1;
-                else
-                    start = s.top();
-                    
-                
-                int curr_area = histogram[top] * (i - start -1);
-                area = max(area, curr_area);
+        int maxArea = 0;
+        for(int i=0; i<histo.size(); i++){
+            int h = histo[i];
+            while(!s.empty() && histo[s.top()] >= h){
+                int idx = s.top(); s.pop();
+                int h = histo[idx];
+                int w = s.empty()? i:i-s.top()-1;
+                int area = h * w;
+                if(area > maxArea)
+                    maxArea = area;
             }
             s.push(i);
         }
-        
-        while(!s.empty()){
-            int top = s.top();
-            s.pop();
-
-            int start;
-            if(s.empty())
-                start = -1;
-            else
-                start = s.top();
-            
-            int curr_area = histogram[top] * (n - start -1);
-            area = max(area, curr_area);
-        }
-        
-        return area;
+        cout << maxArea << endl;
+        return maxArea;
     }
-    
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int m=matrix.size();
-        if(m==0) return 0;
-        int n=matrix[0].size(), result=0;
-        vector<int> histogram(n, 0);
-        
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> histo(n+1, 0);
+        int ret = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(matrix[i][j]=='1')
-                    histogram[j]+=1;
+                if(matrix[i][j] == '1')
+                    histo[j]++;
                 else
-                    histogram[j]=0;
+                    histo[j] = 0;
             }
-            
-            result = max(result, largestArea(histogram));
-            cout<<result<<" ";
+            int area = getMaxRectangle(histo);
+            ret = max(ret, area);
         }
-        return result;
+        return ret;
     }
 };
