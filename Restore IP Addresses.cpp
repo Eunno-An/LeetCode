5be4328feb7a55c8254272fa137e5a0a6a5fa36b
Eunno An <12171647@inha.edu>
2022-05-14 23:23:51 +0900

    Update Restore IP Addresses.cpp

--- Restore IP Addresses.cpp
+++ Restore IP Addresses.cpp
@@ -8,6 +8,18 @@ Memory Usage: 7.2 MB, less than 17.16% of C++ online submissions for Restore IP 
 재귀의 for문을 이중으로 했음. 사실 그럴 필요가 없었는데 함수 틀을 잘못짬.
 그리고 idx+j < s.size()로 해서 계속 답이 안나왔음. idx+j<=s.size(), 즉 등호로 바꿔야 함. 왜냐하면 마지막 문자열이 포함되어야 하기 때문!! 
 255.255.11.135에서 등호가 안들어가면 11.13까지만 보고 그만두게 됨. 잘 생각해보면 i+j번째도 포함되어야 하므로.. 주의!!
+
+2차 22-05-14
+Success
+Time: 22분
+Success
+Details 
+Runtime: 4 ms, faster than 50.10% of C++ online submissions for Restore IP Addresses.
+Memory Usage: 7.9 MB, less than 10.57% of C++ online submissions for Restore IP Addresses.
+회고: 
+왠지 모르겠는데 1차보다는 성능은 안좋지만 훨씬 빨리 풀었음.
+문제를 잘 읽어야 한다. 029는 valid 숫자가 아니다.
+그리고 확실히 오랜만에 짜다보니까 for문에서 매끄럽지 못한게 느껴진다. 2차 코드 첨부하겠음.
 class Solution {
 public:
     /*
@@ -73,6 +85,71 @@ public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> nowIp;
         help(nowIp, s, 0);
+        return ret;
+    }
+};
+
+
+//2차 코드
+class Solution {
+public:
+    /*
+    문제:
+    문자열 s가 주어지면
+    가능한 valid ip를 모두 리턴해라.
+    
+    제한:
+    
+    해결방법:
+    
+    */
+    vector<string> ret;
+    string getIp(vector<string> make){
+        string temp = "";
+        for(int i=0; i<make.size(); i++){
+            temp += make[i];
+            temp += '.';
+        }
+        temp.pop_back();
+        return temp;
+    }
+    void makeIp(vector<string>& make, string s){
+        if(make.size() == 4){
+            if(s.empty())
+                ret.push_back(getIp(make));
+            return;
+        }
+        int num = 0;
+        
+        if(s.size() >= 1){
+            make.push_back(s.substr(0, 1));
+            makeIp(make, s.substr(1));
+            make.pop_back();
+        }
+        if(s.size() >= 2){
+            num = stoi(s.substr(0, 2));
+            if(num >= 10 && num <= 99){
+                make.push_back(to_string(num));
+                makeIp(make, s.substr(2));
+                make.pop_back();
+            }
+        }
+        
+        if(s.size() >= 3){
+            num = stoi(s.substr(0, 3));
+            if(num >= 100 && num <= 255){
+                make.push_back(to_string(num));
+                makeIp(make, s.substr(3));
+                make.pop_back();
+            }
+        }
+        return;
+        
+        
+    }
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> make;
+        makeIp(make, s);
         return ret;
     }
 };
